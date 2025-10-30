468244a86c69ec9b354102fc942cb7ae1d64a695
KayT <kayt1206.js@gmail.com>
2025-10-30 12:48:31 +0700

    Create Inventory

---
+++ Assets/Script/System/PersistentObjects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentObjects : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); // Giữ nguyên toàn bộ GameObject này khi đổi Scene
+    }
+}
