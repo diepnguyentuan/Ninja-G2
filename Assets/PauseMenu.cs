a7f12bb2863cf4ba6b7116a95100cf79cdf05fa5
ductnf8 <ductnf8@fullstack.edu.vn>
2025-11-11 05:29:45 +0700

    Lưu thay đổi tạm từ develop_backup trước khi sang duc

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // thêm dòng này để dùng SceneManager
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; // tạm dừng game
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f; // tiếp tục game
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f; // reset tốc độ thời gian
+    }
+
+    public void Home()
+    {
+        pauseMenu.SetActive(false);
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+    }
+
+    public void ReturnMenu()
+    {
+        Time.timeScale = 0f;
+        SceneManager.LoadScene(0);
+    }
+}
